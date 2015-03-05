cac7eb9f8fb0f78939103c2e84234c20c4f213e9
lmfaoown <acejamshidi4th@gmail.com>
2015-03-05 04:26:07 -0500

    Disgusting Code But Functional
    This adds the use of arguments because I will not be adding a jGUI.  I need to learn how to make custom arguments functional...  Apparently -ARGUMENT:BOOLEAN (e.g. -debug:true) isn't how it's supposed to be.

--- woodcutter/WoodCutter.java
+++ woodcutter/WoodCutter.java
@@ -1,7 +1,7 @@
 /*
  * Script framework example provided by Worthy
  * http://bit.ly/1M1w4sp
- * 1
+ * 
  * Paint example provided by Deltac0
  * http://bit.ly/1F0WwAj
  * 
@@ -14,48 +14,93 @@
 
 package scripts.woodcutter;
 
+import java.awt.Graphics;
+import java.awt.Polygon;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 import org.tribot.api.General;
+import org.tribot.api2007.Login;
+import org.tribot.api2007.Player;
+import org.tribot.api2007.Projection;
+import org.tribot.api2007.types.RSTile;
 import org.tribot.script.Script;
 import org.tribot.script.ScriptManifest;
+import org.tribot.script.interfaces.Arguments;
 import org.tribot.script.interfaces.MessageListening07;
+import org.tribot.script.interfaces.Painting;
 
 import scripts.woodcutter.api.Node;
 import scripts.woodcutter.nodes.Bank;
 import scripts.woodcutter.nodes.CutTree;
 import scripts.woodcutter.nodes.WalkToTrees;
-//import scripts.woodcutter.nodes.FailSafe;
 import scripts.woodcutter.nodes.WalkToBank;
+import scripts.woodcutter.nodes.PublicCalls;
 
-@ScriptManifest (authors = {"Lmfaoown"}, category = "Framework Example", name = "WoodCutter")
-public class WoodCutter extends Script implements MessageListening07 {
-	public boolean loopStatus = true;
-	public static ArrayList<Node> nodes = new ArrayList<>();
+
+/*
+ * Learn how to read text arguments instead of using HTML check boxes
+ * for ease of client-starter
+ */
+@ScriptManifest (authors = {"Lmfaoown"},version = 1.0, category = "Framework Example", name = "WoodCutter", description =	
+"<html><body><form action=\"aa\">"
+		+ "<input type=\"checkbox\" name=\"debug\">Debug mode<br>"
+		+ "<input type=\"checkbox\" name=\"paint\">Paint"
+		//+ 
++ "</form></body></html>")
+public class WoodCutter extends Script implements MessageListening07, Painting, Arguments {
 	
+	private boolean isLooping;
+	private static ArrayList<Node> nodes = new ArrayList<>();
 	
+	boolean debug;
+	boolean paint;
+	//String custommode = "false";
 	
+	@Override
 	public void run() {
 		Collections.addAll(nodes,
 				new CutTree(), 
 				new WalkToBank(),
 				new Bank(),
-				new WalkToTrees());    //add all nodes to the ArrayList
-		loop(20, 40);
+				new WalkToTrees(),
+				new PublicCalls());    //add all nodes to the ArrayList
+		loop(80, 1000);
 	}
 	
 	private void loop(int min, int max) {
-		while (loopStatus) {
-			for (final Node node : nodes) {
+		if(Login.getLoginState() == Login.STATE.LOGINSCREEN)
+			println("Logging in..."); Login.login();
+			
+		//debug = debug.toString();
+		//paint = paint.toString();
+		
+		println("debug is.. " + debug);
+		println("paint is.. " + paint);
+		println("Does debug == true? " + (debug == true));
+		println("Does paint == true? " + (paint == true));
+		
+		//println("custommode is... " + custommode);
+		
+		isLooping = true;
+		
+		while (isLooping)
+			for (final Node node : nodes)
 				if (node.validate()) {
 					node.execute();
 					sleep(General.random(min, max));
 				}
-			}
-		}
 	}
 
+	public boolean getLoopStatus() {
+		return isLooping;
+	}
+	
+	public void setLoopStatus(boolean isLooping) {
+		this.isLooping = isLooping;
+	}
+	
 	@Override
 	public void clanMessageReceived(String arg0, String arg1) {
 		// TODO Auto-generated method stub
@@ -86,7 +131,7 @@ public class WoodCutter extends Script implements MessageListening07 {
 		println(arg0);
 		if(arg0.toLowerCase().contains("an axe to chop")) {
 			println("No axe, stopping and logging out.");
-			loopStatus = false;
+			isLooping = false;
 		}
 	}
 
@@ -94,5 +139,46 @@ public class WoodCutter extends Script implements MessageListening07 {
 	public void tradeRequestReceived(String arg0) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public void passArguments(HashMap<String, String> m) {
+		debug = m.get("debug").equals("true");
+		paint = m.get("paint").equals("true");
+		//custommode = m.get("custommode");
+	}
+	
+	@Override
+	public void onPaint(Graphics g) {
+		//RSObject tree = PublicCalls.findNearest(15, 0, CutTree.TREE_IDS);
+		//ASSIGNED_TREE = new RSTile(0, 0);
+		//println(paint == "true");
+		//println(debug == "true");
+		if(paint == true) {
+			int x = CutTree.ASSIGNED_TREE.getX();
+			int y = CutTree.ASSIGNED_TREE.getY();
+			
+			Polygon t1 = Projection.getTileBoundsPoly(new RSTile(x, y), 0);
+			Polygon t2 = Projection.getTileBoundsPoly(new RSTile(x+1, y), 0);
+			Polygon t3 = Projection.getTileBoundsPoly(new RSTile(x, y+1), 0);
+			Polygon t4 = Projection.getTileBoundsPoly(new RSTile(x+1, y+1), 0);
+			g.drawPolygon(t1); g.drawPolygon(t2); g.drawPolygon(t3); g.drawPolygon(t4);
+		}
+		
+		if(debug == true) {
+			//245, 503
+			int x = 20;
+			int y = 10;
+			g.drawString("Animation:   " + Player.getAnimation(), 	x, y+(24*0));
+			g.drawString("Position:    " + Player.getPosition(), 	x, y+(24*1));
+			
+			g.drawString("Tree Pos:    " + CutTree.ASSIGNED_TREE, 	x, y+(24*2));
+			
+			
+			g.drawString("WalkToTrees: " + WalkToTrees.WTTSTATUS, 	x, y+(24*3));
+			g.drawString("CutTree:     " + CutTree.CTSTATUS, 		x, y+(24*4));
+			g.drawString("WalkToBank:  " + WalkToBank.WTBSTATUS,	x, y+(24*5));
+			g.drawString("Bank:        " + Bank.BSTATUS,			x, y+(24*6));
+			
+		}
 	}
 }
